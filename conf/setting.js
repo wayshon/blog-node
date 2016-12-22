896f463af8701a30089d90f41c0b9292c8932120
王旭 <wangxv@rayeye.cn>
2016-12-22 23:03:40 +0800

    为适应腾讯云，改了临时数据库

--- conf/setting.js
+++ conf/setting.js
@@ -1,8 +1,14 @@
 module.exports = {
-  user: 'test',
-  database: 'blog',
-  host: '127.0.0.1',
-  password: '123456',
+  // user: 'test',
+  // database: 'blog',
+  // host: '127.0.0.1',
+  // password: '123456',
+  // port: 3306,
+  // cookieSecret: 'blog'
+  user: 'xblog_f',
+  database: 'xblog',
+  host: 'tjsomv82.2252.dnstoo.com',
+  password: 'wangxu4099',
   port: 3306,
   cookieSecret: 'blog'
 }; 
